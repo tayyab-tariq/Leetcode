d83cade363eb4061940e7773ef80ea4eed61f271
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-01-05 15:25:02 +0500

    Create Valid Palindrome.py

---
+++ Valid Palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        
+        index = len(s)-1
+        for i in range(len(s)):
+            if not s[i].isalnum():
+                continue
+             
+            while not s[index].isalnum():
+                index -= 1
+            
+            if s[i] != s[index]:
+                return False
+            
+            index -= 1
+            
+            if i > index:
+                break
+        
+        return True
