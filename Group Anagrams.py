0b713bb430ecda1d61975b3192d66e760fe428fb
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-01-27 12:10:46 +0500

    Update Group Anagrams.py

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -1,10 +1,22 @@
 import collections
 class Solution:    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        grouped = collections.defaultdict(list)        
-        for i in strs:
-            sort = " ".join(sorted(i))
-            # print(sort,i)
-            grouped[sort].append(i)
-        return grouped.values()
+        groupings = {}
+
+        for words in strs:
+            word = tuple(sorted(words))
+            if word in groupings.keys():
+                groupings[word].append(words)
+            else:
+                groupings[word] = []
+                groupings[word].append(words)
+
+        return groupings.values()
+    
+#         grouped = collections.defaultdict(list)        
+#         for i in strs:
+#             sort = " ".join(sorted(i))
+#             # print(sort,i)
+#             grouped[sort].append(i)
+#         return grouped.values()
             
