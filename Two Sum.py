a1212a01375bc7b1584626e14f8ed71592a89b98
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-01-27 11:27:02 +0500

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapping = {}
+        mapp = {}
+
+        for i in range(len(nums)):
+        difference = target - nums[i]
+        if nums[i] in mapp.keys():
+            return [mapp.get(nums[i],0), i]
+        mapp[difference] = i
+            
+        # for i,n in enumerate(nums):
+        #     difference = target - n
+        #     if difference in mapp:
+        #         return [mapp[difference], i]
+        #     mapp[n] = i
         
-        for i,n in enumerate(nums):
-            diff = target - n
-            if diff in mapping:
-                return [mapping[diff],i]
-            mapping[n] = i 
+        
+     
