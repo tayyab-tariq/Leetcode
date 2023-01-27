5cb1602cc22cca9f4a31837a49917f98c15de94e
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-01-27 12:26:08 +0500

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -1,20 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums)+1)]
+        frequency = {}
+        for num in nums:
+            frequency[num] = 1 + frequency.get(num,0)
+
+        elements = []
+
+        for i in range(k):
+            max_freq = -1
+            for key, value in frequency.items():
+                if frequency[key] >= max_freq:
+                    if key not in elements:
+                        number = key
+                        max_freq = frequency[key]
+            elements.append(number)
+            frequency[number] = -1
         
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
+        return elements
+    
+#         count = {}
+#         freq = [[] for i in range(len(nums)+1)]
+        
+#         for n in nums:
+#             count[n] = 1 + count.get(n, 0)
             
-        for n, c in count.items():
-            freq[c].append(n)
+#         for n, c in count.items():
+#             freq[c].append(n)
         
-        res = []
-        for i in range(len(freq) - 1,-1,-1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+#         res = []
+#         for i in range(len(freq) - 1,-1,-1):
+#             for n in freq[i]:
+#                 res.append(n)
+#                 if len(res) == k:
+#                     return res
                 
             
     
