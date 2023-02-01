78e5b72d96b4c35e56cfd415041c1955737891fe
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-02-01 11:51:08 +0500

    Update Valid Palindrome.py

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -1,12 +1,24 @@
-
-
-
-
-
-
-
-
-
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        
+        index = len(s)-1
+        for i in range(len(s)):
+            if not s[i].isalnum():
+                continue
+             
+            while not s[index].isalnum():
+                index -= 1
+            
+            if s[i] != s[index]:
+                return False
+            
+            index -= 1
+            
+            if i > index:
+                break
+        
+        return True
 
 # class Solution:
 #     def isPalindrome(self, s: str) -> bool:
