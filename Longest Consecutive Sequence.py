330f576d9b3bfad4acffff8f0a6ee12265898349
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-02-01 11:03:28 +0500

    Update Longest Consecutive Sequence.py

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -16,3 +16,20 @@ class Solution:
             return max(length,longest)
         else:
             return 0
+
+        
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         numSet = set(nums)
+#         longest = 0
+
+#         for num in nums:
+#             if num-1 not in numSet:
+#                 length = 1
+#                 while (num + length) in numSet:
+#                     length += 1
+                
+#                 longest = max(length, longest)
+            
+#         return longest
+           
