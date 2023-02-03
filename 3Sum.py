ce8ee5e2cf99e0b82dc59ff5562b2515d318c0e4
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-02-03 15:51:02 +0500

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -1,26 +1,28 @@
-import numpy as np
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = np.sort(nums)
-        arr = []
-        
-        for i,num in enumerate(nums):
-            if i>0 and nums[i-1]==num:
+        sorted_list = sorted(nums)
+        result = []
+        for i, num in enumerate(sorted_list):
+            if num > 0:
+                break
+            if i > 0 and num == sorted_list[i-1]:
                 continue
-                
-            i,j = i+1,len(nums)-1
-            
-            while i<j:
-                triplet = num + nums[i] + nums[j]
-                if triplet>0:
-                    j-=1
-                elif triplet<0:
-                    i+=1
+
+            left, right = i + 1, len(sorted_list) - 1
+            while left < right:
+                target = num + sorted_list[left] + sorted_list[right]
+                if target < 0:
+                    left += 1
+                elif target > 0:
+                    right -= 1
                 else:
-                    arr.append([num,nums[i],nums[j]])
-                    i+=1
-                    while nums[i] == nums[i-1] and i<j:
-                        i+=1
-                        
-        return arr
+                    result.append([num, sorted_list[left], sorted_list[right]])
+                    left += 1
+                    right -= 1
+                    while sorted_list[left] == sorted_list[left - 1] and left < right:
+                        left += 1
+            
+        
+        return result
+
         
