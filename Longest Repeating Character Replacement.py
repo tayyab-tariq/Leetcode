93d55e3427e26fcc888da47bc603e953ba2c00c3
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-02-08 12:23:23 +0500

    Update Longest Repeating Character Replacement.py

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -1,22 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        char = {}
-        
-        left = 0
-        res = 0
-        maxf = 0
-        
-        for right in range(len(s)):
-            window = right - left + 1
-            char[s[right]] = 1 + char.get(s[right],0)
-            
-            maxf = max(maxf,char[s[right]])
-            
-            if window - maxf > k:
-                char[s[left]] -= 1
-                window -= 1
-                left += 1
-            
-            res = max(res,window)
-            
-        return res 
+        res, left, maxf = 0, 0, 0
+        freq = {}
+        for r,chr in enumerate(s):
+            freq[chr] = 1 + freq.get(chr, 0)
+            maxf = max(maxf, freq[chr])            
+            if ( (r-left+1) - maxf <= k):
+                res += 1
+            else:
+                freq[s[left]] -= 1
+                left += 1     
+        return res
