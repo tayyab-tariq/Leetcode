94f5bc80895150c81be828e4c598d2b4e3424120
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-06-24 03:11:48 +0500

    Create Valid Parenthesis.py

---
+++ Valid Parenthesis.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_bracket = {'[':']', '{':'}', '(':')'}        
+        brackets = []
+        for i in range(len(s)):
+            if s[i] in open_bracket:
+                brackets.append(open_bracket[s[i]])
+            else:
+                if brackets:
+                    if s[i] != brackets.pop():
+                        return False
+                else:
+                    return False
+
+        if brackets:
+            return False
+
+        return True
+
+        
