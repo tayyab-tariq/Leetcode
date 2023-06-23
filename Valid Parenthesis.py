54edadb93d3c9200472963c2cff26454fd029afa
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-06-24 03:12:15 +0500

    Update Valid Parenthesis.py

--- Valid Parenthesis.py
+++ Valid Parenthesis.py
@@ -17,4 +17,20 @@ class Solution:
 
         # return True
 
+        open_bracket = {'{':'}', '(':')', '[':']'}
+        stack = []
+        for bracket in s:
+            if bracket in open_bracket.keys():
+                stack.append(bracket)
+            else:
+                if stack:
+                    closed = stack.pop()
+                    if open_bracket.get(closed, 0) != bracket:
+                        return False
+                else:
+                    return False
         
+        if stack:
+            return False
+        else:
+            return True
