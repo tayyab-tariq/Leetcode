6b3bf6245db629e18f11b39e75f88dfb38ca93d1
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-07-12 15:52:45 +0500

    Update Koko Eating Bananas.py

--- Koko Eating Bananas.py
+++ Koko Eating Bananas.py
@@ -1,17 +1,21 @@
+import math
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int: 
-        curr_speed = ceil(sum(piles) / h)
-        big_piles = [pile for pile in piles if pile > curr_speed]
-        curr_turns = [ceil(pile / curr_speed) for pile in big_piles]
-        curr_num_hours = (len(piles) - len(big_piles)) + sum(curr_turns)
+        curr_speed = ceil(sum(piles)/h)
+        max_piles = [pile for pile in piles if pile > curr_speed]
+        curr_turns = [ceil(pile/curr_speed) for pile in max_piles]
+        speed = sum(curr_turns) + (len(piles) - len(max_piles))
+        if speed > h:
+            curr_speed = ceil((speed/h) * curr_speed)
+        while speed > h:
+            max_piles = [pile for pile in piles if pile > curr_speed]
+            curr_turns = [ceil(pile/curr_speed) for pile in max_piles]
+            speed = sum(curr_turns) + (len(piles) - len(max_piles))
+            if speed > h:
+                curr_speed = min(max_piles)
+                min_index = max_piles.index(curr_speed)
+                curr_speed = min(curr_speed, ceil(max_piles[min_index] / (curr_turns[min_index] - 1)))
 
-        while curr_num_hours > h:
-            next_speed = float("inf")
-            for i in range(len(big_piles)):
-                if curr_turns[i] > 1:
-                    next_speed = min(next_speed, ceil(big_piles[i] / (curr_turns[i] - 1)))
-            curr_speed = next_speed
-           
-            curr_turns = [ceil(pile / curr_speed) for pile in big_piles]
-            curr_num_hours = (len(piles) - len(big_piles)) + sum(curr_turns)
+                          
         return curr_speed
