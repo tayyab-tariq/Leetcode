666243f124b9f9fba2c914abfc4c7ca5249a2ea3
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-07-21 15:38:05 +0500

    Update Find Minimum In Rotated Sorted Array.py

--- Find Minimum In Rotated Sorted Array.py
+++ Find Minimum In Rotated Sorted Array.py
@@ -3,14 +3,12 @@ class Solution:
         left, right = 0, len(nums)-1
         target = nums[0]
 
-        while left < right:
+        while left <= right:
             mid = (left + right) // 2
-            if nums[mid] > nums[right]:
+            if nums[mid] > target:
                 left = mid + 1
             else:
                 right = mid - 1
-
             target = min(nums[mid], target)
-
-        return min(nums[left], target)
-            
+        
+        return min(nums[right], target)
