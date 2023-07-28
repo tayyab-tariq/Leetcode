b78f4f56e0c797c1c47393109ce24ba414a4bd3f
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-07-28 22:15:55 +0500

    Create Linked List Cycle.py

---
+++ Linked List Cycle.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nodes = {}
+        while head:
+            if nodes.get(head, 0):
+                return True
+            else:
+                nodes[head] = True
+            head = head.next
+        
+        return False
