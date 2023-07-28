a7040f9de3ca175e8f028050c92dac9cf4ae9ecd
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-07-28 22:31:47 +0500

    Update Linked List Cycle.py

--- Linked List Cycle.py
+++ Linked List Cycle.py
@@ -4,12 +4,6 @@
 #         self.val = x
 #         self.next = None
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
