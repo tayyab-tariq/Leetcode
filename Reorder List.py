df639cda2496b879b9071d3d985316415cb546e6
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-07-29 23:48:14 +0500

    Create Reorder List.py

---
+++ Reorder List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head.next
+        while slow:
+            if not fast or not fast.next:
+                break
+            slow = slow.next
+            fast = fast.next.next
+        
+        
