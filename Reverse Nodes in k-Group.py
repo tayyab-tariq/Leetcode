5ecc72ecb2978cf66a235e019b75a90db5f2f0c9
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-02 10:49:07 +0500

    Update Reverse Nodes in k-Group.py

--- Reverse Nodes in k-Group.py
+++ Reverse Nodes in k-Group.py
@@ -5,25 +5,24 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        curr, prev = head, None
-        count = 0
-        temp = curr
-        while curr:
-            if (count+1) % k == 0 and k > 1 and curr:          
-                if count == 1:
-                    tmp = curr.next
-                    temp.next = curr.next
-                    curr.next = temp
-                    head = curr
-                    curr = temp
-                    temp = temp.next
-                else:
-                    print(prev.val, curr.val, temp.val)
-            
-
-            prev = curr
+        curr = head
+        for _ in range(k):
+            if not curr: 
+                return head
             curr = curr.next
-            count += 1
+        
+        prev = None
+        curr = head
+        for _ in range(k):
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        second = prev
+        while second.next:
+            second = second.next
 
-        return head
-            
+        second.next = self.reverseKGroup(curr, k)
+        
+        return prev
