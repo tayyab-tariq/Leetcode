5ec97b1e4bfe70ae6c922b28b2ae44027d60db48
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-02 11:58:56 +0500

    Update Reverse Nodes in k-Group.py

--- Reverse Nodes in k-Group.py
+++ Reverse Nodes in k-Group.py
@@ -5,12 +5,15 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        #   Check if k nodes exist for reversal
         curr = head
         for _ in range(k):
             if not curr: 
                 return head
             curr = curr.next
         
+        #   Reverse k number of nodes
         prev = None
         curr = head
         for _ in range(k):
@@ -19,10 +22,10 @@ class Solution:
             prev = curr
             curr = temp
         
-        second = prev
-        while second.next:
-            second = second.next
 
-        second.next = self.reverseKGroup(curr, k)
+        #   curr contains remaing original nodes which are not reversed
+        #   Point the tail node to reversed nodes
+        #   head contains the tail node
+        head.next = self.reverseKGroup(curr, k)
         
         return prev
