e5032844c31ab683e27c4519523e08589c62510a
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-04 00:44:17 +0500

    Update LRU Cache.py

--- LRU Cache.py
+++ LRU Cache.py
@@ -8,15 +8,21 @@ class ListNode:
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
+        self.capacity = capacity
+        self.count = 0
+        self.head = ListNode(0)
         
     def get(self, key: int) -> int:
-        return self.cache.get(key, -1)
+        return -1
 
     def put(self, key: int, value: int) -> None:
         # if len(self.cache) > capacity:
-            
-        self.cache[key] = value
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = val
+
+        self.count += 1    
 
 
 # Your LRUCache object will be instantiated and called as such:
