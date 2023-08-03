460539d5349e4ec46507fa1d33f6aa0da6102973
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-03 16:53:29 +0500

    Update Merge k Sorted Lists.py

--- Merge k Sorted Lists.py
+++ Merge k Sorted Lists.py
@@ -39,4 +39,28 @@ class Solution:
             tail.next = list1 if list1 else list2
             
         return node.next
+
+
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         if not lists:
+#             return None
+#         if len(lists) == 1:
+#             return lists[0]
+#         mid = len(lists) // 2
+#         l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+#         return self.merge(l, r)
+    
+#     def merge(self, l, r):
+#         dummy = p = ListNode()
+#         while l and r:
+#             if l.val < r.val:
+#                 p.next = l
+#                 l = l.next
+#             else:
+#                 p.next = r
+#                 r = r.next
+#             p = p.next
+#         p.next = l or r
+#         return dummy.next
     
