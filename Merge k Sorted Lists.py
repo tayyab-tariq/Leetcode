e1fd45f4b70d10cd32f68da530f5e56858979830
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-03 13:14:50 +0500

    Update Merge k Sorted Lists.py

--- Merge k Sorted Lists.py
+++ Merge k Sorted Lists.py
@@ -23,7 +23,7 @@ class Solution:
         node = ListNode()
         tail = node
 
-        while list1 or list2:
+        while list1 and list2:
             if list1 and list2:
                 if list1.val < list2.val:
                     tail.next = list1
@@ -34,14 +34,9 @@ class Solution:
                     list2 = list2.next
 
                 tail = tail.next
-            elif list1:
-                tail.next = list1
-                tail = tail.next
-                list1 = list1.next
-            elif list2:
-                tail.next = list2
-                tail = tail.next
-                list2 = list2.next
+        
+        if list1 or list2:
+            tail.next = list1 if list1 else list2
             
         return node.next
     
