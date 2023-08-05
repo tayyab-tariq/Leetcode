0c984cf47cf34e05c2624f9e1b0cb586f98f8dc6
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-05 22:17:11 +0500

    Update LRU Cache.py

--- LRU Cache.py
+++ LRU Cache.py
@@ -26,3 +26,16 @@ def set(self, key, value):])
     if len(self.dic) > self.capacity:
         n = self.head.next
         del self.dic[n.key]
+
+def _remove(self, node):
+    p = node.prev
+    n = node.next
+    p.next = n
+    n.prev = p
+
+def _add(self, node):
+    p = self.tail.prev
+    p.next = node
+    self.tail.prev = node
+    node.prev = p
+    node.next = self.tail
