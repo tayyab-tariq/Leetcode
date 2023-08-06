b739377f3d0f057ffdd496fa255d7adc12a2a6d4
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-06 23:00:08 +0500

    Update Copy List with Random Pointer.py

--- Copy List with Random Pointer.py
+++ Copy List with Random Pointer.py
@@ -9,3 +9,11 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldToCopy = {None: None}
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+        cur = head
