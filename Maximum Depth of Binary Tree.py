f62f9b054dcb26e538a02ccd692c4eec3d6cded2
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-11 01:23:08 +0500

    Update Maximum Depth of Binary Tree.py

--- Maximum Depth of Binary Tree.py
+++ Maximum Depth of Binary Tree.py
@@ -13,3 +13,19 @@ class Solution:
             return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
 
         return dfs(root, 0)
+
+    # def maxDepth(root, depth):            #        BFS
+        #     if not root:
+        #         return depth
+            
+        #     queue = [(root, 1)]
+        #     while queue:
+        #         node, depth = queue.pop(0)
+        #         if node.left:
+        #             queue.append((node.left, depth + 1))
+        #         if node.right:
+        #             queue.append((node.right, depth + 1))                
+        #     return depth
+
+        # return maxDepth(root, 0)
+
