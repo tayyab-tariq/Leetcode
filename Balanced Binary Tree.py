5e4893ea190ddab6f7af77fea5fb2b8a9e2971b5
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-14 23:18:08 +0500

    Update Balanced Binary Tree.py

--- Balanced Binary Tree.py
+++ Balanced Binary Tree.py
@@ -1,4 +1,3 @@
-View on Github
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,20 +6,12 @@ View on Github
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root):
+        def height(root):
             if not root:
-                return [True, 0]
-
-            left, right = dfs(root.left), dfs(root.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [balanced, 1 + max(left[1], right[1])]
-
-        return dfs(root)[0]
-
-    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
-    #     return (self.Height(root) >= 0)
-    # def Height(self, root: Optional[TreeNode]) -> bool:
-    #     if root is None:  return 0
-    #     leftheight, rightheight = self.Height(root.left), self.Height(root.right)
-    #     if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
-    #     return max(leftheight, rightheight) + 1
+                return 0
+            
+            leftheight, rightheight = height(root.left), height(root.right)
+            if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
+            return max(leftheight, rightheight) + 1
+        
+        return height(root)
