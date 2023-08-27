bba6dbc8b0ff8a840fa6b50f098c99e7335521f6
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-27 22:31:58 +0500

    Update Binary Tree Level Order Traversal.py

--- Binary Tree Level Order Traversal.py
+++ Binary Tree Level Order Traversal.py
@@ -12,3 +12,15 @@ class Solution:
         queue = []
         return_list = []
         queue.append(root)
+        while len(queue) > 0:
+            ans = []
+            l = len(queue)
+            for l in range(l):
+                node = queue.pop(0)
+                ans.append(node.val)
+                if node.left != None:
+                    queue.append(node.left)
+                if node.right != None:
+                    queue.append(node.right)
+            return_list.append(ans)
+        return return_list
