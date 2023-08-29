0504d182ed6467ac5ee17de69510c227dcb784d1
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-29 19:02:29 +0500

    Update Binary Tree Right Side View.py

--- Binary Tree Right Side View.py
+++ Binary Tree Right Side View.py
@@ -7,3 +7,8 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        return [root.val] + right + left[len(right):]
