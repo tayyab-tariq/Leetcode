409e7dd7b900a9b523d4bb1be5744799e05c2790
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-29 19:03:09 +0500

    Update Binary Tree Right Side View.py

--- Binary Tree Right Side View.py
+++ Binary Tree Right Side View.py
@@ -12,3 +12,24 @@ class Solution:
         right = self.rightSideView(root.right)
         left = self.rightSideView(root.left)
         return [root.val] + right + left[len(right):]
+
+
+
+
+        # res = []
+        # q = collections.deque()
+        # if root:
+        #     q.append(root)
+
+        # while q:
+        #     val = []
+
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         val.append(node.val)
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     res.append(val)
+        # return res
