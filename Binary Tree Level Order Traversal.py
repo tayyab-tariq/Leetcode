ffc296b2e38b386f5447b4c2c1c8320d2f1b47e1
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-08-31 16:33:42 +0500

    Update Binary Tree Level Order Traversal.py

--- Binary Tree Level Order Traversal.py
+++ Binary Tree Level Order Traversal.py
@@ -7,20 +7,23 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
-            return root
-        queue = []
-        return_list = []
-        queue.append(root)
-        while len(queue) > 0:
+        if not root:
+            return []
+        
+        level_order = []
+        local = [root]
+    
+        while local:
             ans = []
-            l = len(queue)
-            for l in range(l):
-                node = queue.pop(0)
+            for i in range(len(local)):
+                node = local.pop(0)
                 ans.append(node.val)
-                if node.left != None:
-                    queue.append(node.left)
-                if node.right != None:
-                    queue.append(node.right)
-            return_list.append(ans)
-        return return_list
+
+                if node.left:
+                    local.append(node.left)
+                if node.right:
+                    local.append(node.right)
+            level_order.append(ans)
+        
+        return level_order
+                
