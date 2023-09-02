2081bbbce1b5e5b0d7f12570156e43a73ae404f2
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-09-02 20:35:00 +0500

    Update Subsets.py

--- Subsets.py
+++ Subsets.py
@@ -1,1 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
 
+        for i in range(len(nums)):
+            subsets = []
+            for j in range(len(res)):
+                subset = res[j] + [nums[i]]
+                subsets.append(subset)
+            res.extend(subsets)
+        return res
