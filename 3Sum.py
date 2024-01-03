ae397057f204298a89eab2dd28e3d969508c2224
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2024-01-04 01:38:25 +0500

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -1,49 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sorted_list = sorted(nums)
-        # result = []
-        # for i, num in enumerate(sorted_list):
-        #     if num > 0:
-        #         break
-        #     if i > 0 and num == sorted_list[i-1]:
-        #         continue
-
-        #     left, right = i + 1, len(sorted_list) - 1
-        #     while left < right:
-        #         target = num + sorted_list[left] + sorted_list[right]
-        #         if target < 0:
-        #             left += 1
-        #         elif target > 0:
-        #             right -= 1
-        #         else:
-        #             result.append([num, sorted_list[left], sorted_list[right]])
-        #             left += 1
-        #             right -= 1
-        #             while sorted_list[left] == sorted_list[left - 1] and left < right:
-        #                 left += 1
-            
         
-        # return result
+        res = set()
+        nums = sorted(nums)
 
-        sorted_nums = sorted(nums)
-        result = set()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
 
-        for i, num in enumerate(sorted_nums):
-            if num > 0:
-                return result
-            left, right = i+1, len(sorted_nums)-1
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left, right, target = i+1, len(nums)-1, 0-nums[i]
             while left < right:
-                target = sorted_nums[i] + sorted_nums[left] + sorted_nums[right]
-                if target == 0:
-                    result.add(tuple([sorted_nums[i], sorted_nums[left], sorted_nums[right]]))
-                    left += 1
+                num = nums[left] + nums[right]
+                if num > target:
                     right -= 1
-
-                elif target < 0:
+                elif num < target:
                     left += 1
                 else:
+                    res.add((nums[left], nums[i], nums[right]))
+                    left += 1
                     right -= 1
 
-        return result
 
-        
+        return res
